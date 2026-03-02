497a28f6c57a26455853469bc1df8a73c5d49c70
Otto <editingkhuduga@gmail.com>
2026-03-02 14:10:00 +0200

    color mode fix

--- src/main.tsx
+++ src/main.tsx
@@ -2,17 +2,24 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-import { ChakraProvider } from "@chakra-ui/react";
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
 import App from "./App";
 import "./index.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
+import { config } from "mathjs";
 
 const client = new QueryClient();
+const theme = extendTheme({
+  config: {
+    initialColorMode: "light",
+    useSystemColorMode: false,
+  },
+});
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <QueryClientProvider client={client}>
-      <ChakraProvider>
+      <ChakraProvider theme={theme}>
         <App />
         <ReactQueryDevtools />
       </ChakraProvider>
